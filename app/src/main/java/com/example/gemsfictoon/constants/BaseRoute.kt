c883806071a328e59bebcfba94f5a6ec3811fff2
Wasabi199 <xxryan0909@gmail.com>
2024-03-03 12:59:40 +0800

    Update BaseRoute.kt

--- app/src/main/java/com/example/gemsfictoon/constants/BaseRoute.kt
+++ app/src/main/java/com/example/gemsfictoon/constants/BaseRoute.kt
@@ -1,5 +1,6 @@
 package com.example.gemsfictoon.constants
 
 object BaseRoute {
-    const val BASE_URL = "http://192.168.100.244:8000/api/"
+//    const val BASE_URL = "http://192.168.100.244:8000/api/" local
+    const val BASE_URL = "http://45.93.138.214/api/"  //deployed
 }
